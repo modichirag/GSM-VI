8f752dc9888c2c791ff268a71ce8919bbf2c48aa
modichirag <cmodi@flatironinstitute.org>
2023-12-30 13:43:39 -0500

    mean field gsm

---
+++ examples/example_gsm_mf.py
@@ -0,0 +1,69 @@
+## Most basic example for fitting a target Multivariate Gaussian distribution with GSM updates
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"]="false"
+os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"]="platform"
+
+# enable 16 bit precision for jax
+from jax import config
+config.update("jax_enable_x64", True)
+
+import jax
+import jax.numpy as jnp
+from jax import jit, grad, random
+
+import numpyro
+import numpyro.distributions as dist
+
+# Import GSM
+from gsmvi.gsm_mf import GSM_MF
+from gsmvi.monitors import KLMonitor
+#####
+
+
+#####
+def setup_model(D=10):
+   
+    # setup a Gaussian target distribution
+    mean = np.random.random(D)
+    L = np.random.normal(size = D**2).reshape(D, D)
+    cov = np.matmul(L, L.T) + np.eye(D)*1e-3
+    #sigmasq = np.random.uniform(0, 2, D)
+    #cov = np.diag(sigmasq)
+    model = dist.MultivariateNormal(loc=mean, covariance_matrix=cov)
+    lp = jit(lambda x: jnp.sum(model.log_prob(x)))
+    lp_g = jit(grad(lp, argnums=0))
+
+    return model, mean, cov, lp, lp_g
+
+
+
+if __name__=="__main__":
+    
+    D = 5
+    model, mean, cov, lp, lp_g = setup_model(D=D)
+    ref_samples = model.sample(random.PRNGKey(99), (1000,))
+
+    niter = 1000
+    batch_size = 8
+    gsm = GSM_MF(D=D, lp=lp, lp_g=lp_g)
+    key = random.PRNGKey(99)
+    monitor = KLMonitor(batch_size=32, ref_samples=ref_samples, checkpoint=10, savepoint=1000,\
+                        savepath='./tmp/', plot_samples=True)
+    mean_fit, cov_fit = gsm.fit(key, niter=niter, batch_size=batch_size, monitor=monitor)
+
+    print()
+    print("True mean : ", mean)
+    print("Fit mean  : ", mean_fit)
+    print()
+    print("Check mean fit")
+    print(np.allclose(mean, mean_fit))
+
+    print()
+    print("Check cov fit")
+    print(cov)
+    print(np.diag(cov))
+    print(cov_fit)
+    print(np.allclose(np.diag(cov), cov_fit))
---
+++ gsmvi/gsm_mf.py
@@ -0,0 +1,147 @@
+import jax
+import jax.numpy as jnp
+from jax import jit, random
+#from numpyro.distributions import MultivariateNormal  ##Needed if sampling from numpyro dist below
+import numpy as np              
+
+@jit
+def _gsm_update_single(sample, v, mu0, S0):
+    '''returns GSM update to mean and covariance matrix for a single sample
+    '''
+    S0v = S0 * v
+    vSv = v * S0 * v 
+    mu_v = (mu0 - sample) * v
+    rho = 0.5 * jnp.sqrt(1 + 4*(vSv + mu_v**2)) - 0.5
+    eps0 = S0v - mu0 + sample
+
+    #mu update
+    mu_vT = (mu0 - sample) * v
+    den = 1 + rho + mu_v
+    I = jnp.ones(sample.shape[0])
+    mu_update = 1/(1 + rho) * ( I - mu_vT / den) * eps0
+    mu = mu0 + mu_update
+
+    #S update
+    Supdate_0 =  (mu0-sample) **2 
+    Supdate_1 =  (mu-sample) **2
+    S_update = (Supdate_0 - Supdate_1)
+    return mu_update, S_update
+
+
+@jit
+def gsm_update(samples, vs, mu0, S0):
+    """
+    Returns updated mean and covariance matrix with GSM updates.
+    For a batch, this is simply the mean of updates for individual samples.
+
+    Inputs:
+      samples: Array of samples of shape BxD where B is the batch dimension
+      vs : Array of score functions of shape BxD corresponding to samples
+      mu0 : Array of shape D, current estimate of the mean
+      S0 : Array of shape DxD, current estimate of the covariance matrix
+
+    Returns:
+      mu : Array of shape D, new estimate of the mean
+      S : Array of shape DxD, new estimate of the covariance matrix
+    """
+        
+    assert len(samples.shape) == 2
+    assert len(vs.shape) == 2
+
+    vgsm_update = jax.vmap(_gsm_update_single, in_axes=(0, 0, None, None))
+    mu_update, S_update = vgsm_update(samples, vs, mu0, S0)
+    mu_update = jnp.mean(mu_update, axis=0)
+    S_update = jnp.mean(S_update, axis=0)
+    mu = mu0 + mu_update
+    S = S0 + S_update
+
+    return mu, S
+
+
+
+class GSM_MF:
+    """
+    Wrapper class for using GSM updates to fit a distribution
+    """
+    def __init__(self, D, lp, lp_g):
+        """
+        Inputs:
+          D: (int) Dimensionality (number) of parameters
+          lp : Function to evaluate target log-probability distribution. 
+               (Only used in monitor, not for fitting)
+          lp_g : Function to evaluate score, i.e. the gradient of the target log-probability distribution
+        """
+        self.D = D
+        self.lp = lp
+        self.lp_g = lp_g
+
+        
+    def fit(self, key, mean=None, cov=None, batch_size=2, niter=5000, nprint=10, verbose=True, check_goodness=True, monitor=None):
+        """
+        Main function to fit a multivariate Gaussian distribution to the target
+
+        Inputs:
+          key: Random number generator key (jax.random.PRNGKey)
+          mean : Optional, initial value of the mean. Expected None or array of size D
+          cov : Optional, initial value of the covariance matrix. Expected None or array of size DxD
+          batch_size : Optional, int. Number of samples to match scores for at every iteration
+          niter : Optional, int. Total number of iterations 
+          nprint : Optional, int. Number of iterations after which to print logs
+          verbose : Optional, bool. If true, print number of iterations after nprint
+          check_goodness : Optional, bool. Recommended. Wether to check floating point errors in covariance matrix update
+          monitor : Optional. Function to monitor the progress and track different statistics for diagnostics. 
+                    Function call should take the input tuple (iteration number, [mean, cov], lp, key, number of grad evals).
+                    Example of monitor class is provided in utils/monitors.py
+
+        Returns:
+          mu : Array of shape D, fit of the mean
+          cov : Array of shape DxD, fit of the covariance matrix
+        """
+        if mean is None:
+            mean = jnp.zeros(self.D)
+        if cov is None:
+            cov = jnp.ones(self.D)
+
+        nevals = 1 
+            
+        for i in range(niter + 1):
+            if (i%(niter//nprint) == 0) and verbose : 
+                print(f'Iteration {i} of {niter}')
+                
+            if monitor is not None:
+                if (i%monitor.checkpoint) == 0:
+                    monitor(i, [mean, np.diag(cov)], self.lp, key, nevals=nevals)
+                    nevals = 0
+                    
+            # Can generate samples from jax distribution (commented below), but using numpy is faster
+            key, key_sample = random.split(key, 2) 
+            np.random.seed(key_sample[0])
+            samples = np.random.multivariate_normal(mean=mean, cov=np.diag(cov), size=batch_size)
+            # samples = MultivariateNormal(loc=mean, covariance_matrix=cov).sample(key, (batch_size,))
+            vs = self.lp_g(samples)
+            mean_new, cov_new = gsm_update(samples, vs, mean, cov)
+            nevals += batch_size
+            
+            is_good = self._check_goodness(cov_new)
+            if is_good:
+                mean, cov = mean_new, cov_new
+            else:
+                if verbose: print("Bad update for covariance matrix. Revert")
+                    
+        if monitor is not None:
+            monitor(i, [mean, np.diag(cov)], self.lp, key, nevals=nevals)                    
+        return mean, cov
+
+    
+    def _check_goodness(self, cov):
+        '''
+        Internal function to check if the new covariance matrix is a valid covariance matrix. 
+        Required due to floating point errors in updating the convariance matrix directly, 
+        insteead of it's Cholesky form. 
+        '''
+        is_good = False
+        if np.any(cov <=0):
+            is_good = False
+        else:
+            is_good = True
+        return is_good
