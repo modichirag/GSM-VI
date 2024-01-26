a0b881528c74ad7b3aa695939e2ab45ec4d86e7b
modichirag <cmodi@flatironinstitute.org>
2024-01-26 14:22:40 -0500

    debug reg0 in setup_reg

--- scripts/pdbexp.py
+++ scripts/pdbexp.py
@@ -13,13 +13,14 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--alg', type=str, help='which algorithm to run')
-parser.add_argument('-modeln', type=int, help='model number for PosteriorDB')
+parser.add_argument('--modeln', type=int, help='model number for PosteriorDB')
 #arguments for GSM
 parser.add_argument('--seed', type=int, default=99, help='seed between 0-999, default=99')
 parser.add_argument('--niter', type=int, default=1001, help='number of iterations in training')
 parser.add_argument('--batch', type=int, default=2, help='batch size, default=2')
 parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
-parser.add_argument('--reg', type=float, default=1e-5, help='regularizer for ngd')
+parser.add_argument('--reg', type=float, default=1e-2, help='regularizer for ngd and lsgsm')
+parser.add_argument('--lambdat', type=int, default=0, help='which regularizer to use')
 #arguments for path name
 parser.add_argument('--suffix', type=str, default="", help='suffix, default=""')
 
@@ -51,7 +52,7 @@ D, lp, lp_g, ref_samples = setup_pdb(args.modeln)
 
 basepath = "/mnt/ceph/users/cmodi/ls-gsm/"
 path = f"{basepath}/PDB_{args.modeln}/"
-print(f"save in : {path}")
+print(f"Parent folder in : {path}")
 print("For algorithm : ", alg)
 print("For lr and batch : ", lr, batch_size)
 
@@ -65,33 +66,41 @@ x0 = np.random.random(D).astype(np.float32)*0.1
 if alg == 'gsm':
     gsm = GSM(D=D, lp=lp, lp_g=lp_g)
     path = f"{path}/{alg}/B{batch_size}/S{seed}/"    
+    print(f"save in : {path}")
     mean_fit, cov_fit = gsm.fit(key, batch_size=batch_size,
                                 niter=args.niter, monitor=monitor)
 
 elif alg == 'advi':
     advi = ADVI(D=D, lp=lp)
-    path = f"{path}/{alg}/B{batch_size}-lr{lr:0.3f}/S{seed}/"       
     opt = optax.adam(learning_rate=lr)
+    path = f"{path}/{alg}/B{batch_size}-lr{lr:0.3f}/S{seed}/"       
+    print(f"save in : {path}")
     mean_fit, cov_fit, losses = advi.fit(key, opt, batch_size=batch_size, 
                                          niter=args.niter, monitor=monitor)
 
 elif alg == 'ngd':
     ngd = NGD(D=D, lp=lp, lp_g=lp_g)
     path = f"{path}/{alg}/B{batch_size}-lr{lr:0.3f}-reg{args.reg:0.3f}/S{seed}/"    
+    print(f"save in : {path}")
     mean_fit, cov_fit = ngd.fit(key, lr=args.lr, batch_size=batch_size,
                                 reg=args.reg, niter=args.niter, monitor=monitor)
     
 
 elif alg == 'lsgsm':
     lsgsm = LS_GSM(D=D, lp=lp, lp_g=lp_g)
-    path = f"{path}/{alg}/B{batch_size}-reg{args.reg:0.2f}/S{seed}/"    
-    mean_fit, cov_fit = lsgsm.fit(key, reg=args.reg, batch_size=batch_size, mean=x0,
+    regf = setup_regularizer(args.reg)[args.lambdat]
+    path = f"{path}/{alg}/B{batch_size}-lambdat{args.lambdat}-reg{args.reg:0.2f}/S{seed}/"
+    print(f"save in : {path}")
+    mean_fit, cov_fit = lsgsm.fit(key, regf=regf, batch_size=batch_size, mean=x0,
                                 niter=args.niter, monitor=monitor)
    
 
 os.makedirs(path, exist_ok=True)
 np.save(f"{path}/mean", mean_fit)
 np.save(f"{path}/cov", cov_fit)
+np.save(f"{path}/means", monitor.means)
+np.save(f"{path}/covs", monitor.covs)
+np.save(f"{path}/iparams", monitor.iparams)
 np.save(f"{path}/nevals", monitor.nevals)
 np.save(f"{path}/rkl", monitor.rkl)
 np.save(f"{path}/fkl", monitor.fkl)
--- scripts/setup_regs.py
+++ scripts/setup_regs.py
@@ -2,22 +2,22 @@ import numpy as np
 from gsmvi.ls_gsm import Regularizers
 
 
-def setup_regularizer(reg0):
+def setup_regularizer(reg_init):
 
     # default lambda_t
     regularizer = Regularizers()
 
     # 0: batch_size
     #print("Using lambda_t = c")
-    reg0 = regularizer.constant(reg0)
+    reg0 = regularizer.constant(reg_init)
 
     # 1: batch_size / (t + 1)
     #print("Using lambda_t = c/t")
-    reg1 = regularizer.custom(lambda t : reg0/t)
+    reg1 = regularizer.custom(lambda t : reg_init/t)
 
     # 1: batch_size / (t + 1)
     #print("Using lambda_t = c/\sqrt{t}")
-    reg2 = regularizer.custom(lambda t : reg0/t**0.5)
+    reg2 = regularizer.custom(lambda t : reg_init/t**0.5)
 
     #print("Using lambda_t = B/(D*(t+1))")
     regs = [reg0, reg1, reg2]
