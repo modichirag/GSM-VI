89ff013e51adfc3bbff900d1dabcacf4cbb6953f
modichirag <cmodi@flatironinstitute.org>
2024-10-04 00:33:28 -0400

    exact and dianas upodates

--- gsmvi/em_lr_projection.py
+++ gsmvi/em_lr_projection.py
@@ -125,48 +125,201 @@ def fit_lr_gaussian(data, num_of_latents,
 
 
 
+# @jit
+# def em_exact(llambda, psi, mu, cov):
+#     D, r = llambda.shape#[0], llambda.shape[1]
+#     psi_inv = jnp.diag(jnp.diagonal(psi)**-1)
+#     alpha = llambda.T@psi_inv    
+#     beta = jnp.linalg.pinv(np.identity(r) + alpha@llambda)
+
+#     gamma = cov@(alpha.T@beta.T)
+#     llambda_update = gamma @ jnp.linalg.pinv(beta + beta@alpha@gamma) 
+    
+#     A = jnp.eye(D) - llambda_update @beta @ llambda.T @psi_inv
+#     M = A@cov@A.T + llambda_update @beta @llambda_update.T
+#     psi_update = jnp.diag(jnp.diagonal(M))
+#     return llambda_update, psi_update
+
+
+# #using mle for training
+# def project_lr_gaussian(mu, cov, llambda, psi, num_of_itr=10, 
+#                         tolerance=0.001, diagnosis=False, 
+#                         data=None):
+        
+#     #cap the maximum number of iterations
+#     #initialize
+#     lower_bound_old=None
+#     lower_bound=None
+#     counter=0
+#     while counter < num_of_itr:
+        
+#         llambda, psi = em_exact(llambda, psi, mu, cov)
+            
+#         #use lower bound to determine if converged if data is given
+#         if data is not None:
+#             lower_bound_old=lower_bound
+#             lower_bound=get_lower_bound(data, mu, llambda, psi)            
+#             if lower_bound_old and np.abs(lower_bound/lower_bound_old-1)<tolerance:
+#                 if diagnosis:
+#                     print(f'{counter} iterations to reach convergence\n')
+#                 return llambda, psi
+            
+#         counter+=1
+    
+#     if diagnosis:
+#         print(f'{counter} iterations to reach convergence\n')
+        
+#     return llambda, psi
+
+
+
 @jit
-def em_exact(llambda, psi, mu, cov):
-    D, r = llambda.shape#[0], llambda.shape[1]
+def em_exact(llambda, psi, cov):
+    D, K = llambda.shape
     psi_inv = jnp.diag(jnp.diagonal(psi)**-1)
     alpha = llambda.T@psi_inv    
-    beta = jnp.linalg.pinv(np.identity(r) + alpha@llambda)
+    beta = jnp.linalg.pinv(np.identity(K) + alpha@llambda)
 
     gamma = cov@(alpha.T@beta.T)
     llambda_update = gamma @ jnp.linalg.pinv(beta + beta@alpha@gamma) 
-    
     A = jnp.eye(D) - llambda_update @beta @ llambda.T @psi_inv
     M = A@cov@A.T + llambda_update @beta @llambda_update.T
     psi_update = jnp.diag(jnp.diagonal(M))
+    
     return llambda_update, psi_update
 
 
-#using mle for training
-def project_lr_gaussian(mu, cov, llambda, psi, num_of_itr=10, 
-                        tolerance=0.001, diagnosis=False, 
-                        data=None):
+
+@jit
+def lower_bound_kl(cov, psi, llambda):
+    A = psi + llambda@llambda.T #+ 1e-6*np.eye(psi.size)
+    logabsdet = jnp.linalg.slogdet(A)[1]
+    kl = jnp.trace(cov@jnp.linalg.pinv(A)) + logabsdet
+    return kl
+
+
+
+def fit_lr_gaussian_law(cov, num_of_latents, eta=1.0,
+                tolerance=0.001,num_of_itr=100, diagnosis=False, 
+                        llambda=None, psi=None, nprint=100):
         
-    #cap the maximum number of iterations
+
+    D = cov.shape[0]
+    K = num_of_latents
+    if llambda is None:
+        llambda = np.random.normal(0, 1, size=(D, K))
+    if psi is None:
+        psi = np.diag(np.random.random(D))
+
+
     #initialize
     lower_bound_old=None
     lower_bound=None
     counter=0
-    while counter < num_of_itr:
+    losses = []
+    psis, llambdas = [], []
+    llambdas.append(llambda)
+    psis.append(np.diag(psi))
+    lower_bound=lower_bound_kl(cov, psi, llambda)
+    losses.append(lower_bound)
         
-        llambda, psi = em_exact(llambda, psi, mu, cov)
+    while counter < num_of_itr:
+
+        if (counter%(num_of_itr//nprint) == 0) : print(counter, lower_bound)
+        llambda_update, psi_update = em_exact(llambda, psi,  cov)
+        psi = (1 - eta)*psi + eta*psi_update
+        llambda = (1 - eta)*llambda  + eta*llambda_update
+        
+        #use lower bound to determine if convergeda
+        lower_bound_old=lower_bound
+        lower_bound=lower_bound_kl(cov, psi, llambda)
             
-        #use lower bound to determine if converged if data is given
-        if data is not None:
-            lower_bound_old=lower_bound
-            lower_bound=get_lower_bound(data, mu, llambda, psi)            
-            if lower_bound_old and np.abs(lower_bound/lower_bound_old-1)<tolerance:
-                if diagnosis:
-                    print(f'{counter} iterations to reach convergence\n')
-                return llambda, psi
+        if lower_bound_old and np.abs(lower_bound/lower_bound_old-1)<tolerance:
+            if diagnosis:
+                print(f'{counter} iterations to reach convergence\n')
+            return psi, llambda, [psis, llambdas, losses]
             
+        llambdas.append(llambda)
+        psis.append(np.diag(psi))
+        losses.append(lower_bound)
         counter+=1
     
     if diagnosis:
         print(f'{counter} iterations to reach convergence\n')
         
-    return llambda, psi
+    return psi, llambda, [psis, llambdas, losses]
+
+
+@jit
+def get_diag(u, v):
+        return jax.vmap(jnp.dot, in_axes=[0, 0])(u, v)
+    
+
+
+@jit
+def em_diana(llambda, psi, cov):
+    D, K = llambda.shape
+    psi_inv = jnp.diag(jnp.diagonal(psi)**-1)
+    Id_K = jnp.identity(K)
+    Id_D = jnp.identity(D)
+    
+    alpha = llambda.T@psi_inv 
+    beta = alpha @ (Id_D - llambda@jnp.linalg.pinv(Id_K + alpha@llambda) @alpha)    
+    SBT = (cov@beta.T)
+    llambda_update = SBT@jnp.linalg.pinv(beta@SBT + Id_K - beta@llambda)
+    psi_update = jnp.diag(jnp.diagonal((cov - llambda_update@SBT.T)))
+    
+    return llambda_update, psi_update
+                          
+    
+    
+def fit_lr_gaussian_diana(cov, num_of_latents, eta=1.0,
+                tolerance=0.001,num_of_itr=100, diagnosis=False, 
+                          llambda=None, psi=None, nprint=100):
+
+    D = cov.shape[0]
+    K = num_of_latents
+    if llambda is None:
+        llambda = np.random.normal(0, 1, size=(D, K))
+    if psi is None:
+        psi = np.diag(np.random.random(D))
+
+
+    #cap the maximum number of iterations
+    #initialize
+    lower_bound_old=None
+    lower_bound=None
+    counter=0
+    losses = []
+    psis, llambdas = [], []
+    
+    llambdas.append(llambda)
+    psis.append(np.diag(psi))
+    lower_bound=lower_bound_kl(cov, psi, llambda)
+    losses.append(lower_bound)
+    
+    while counter < num_of_itr:
+
+        if (counter%(num_of_itr//nprint) == 0) : print(counter, lower_bound)
+        llambda_update, psi_update = em_diana(llambda, psi, cov)
+        psi = (1 - eta)*psi + eta*psi_update
+        llambda = (1 - eta)*llambda  + eta*llambda_update
+
+        #use lower bound to determine if convergeda
+        lower_bound_old=lower_bound
+        lower_bound=lower_bound_kl(cov, psi, llambda)
+            
+        if lower_bound_old and np.abs(lower_bound/lower_bound_old-1)<tolerance:
+            if diagnosis:
+                print(f'{counter} iterations to reach convergence\n')
+            return psi, llambda, [psis, llambdas, losses]
+            
+        llambdas.append(llambda*1.)
+        psis.append(np.diag(psi))
+        losses.append(lower_bound)
+        counter+=1
+    
+    if diagnosis:
+        print(f'{counter} iterations to reach convergence\n')
+        
+    return psi, llambda, [psis, llambdas, losses]
