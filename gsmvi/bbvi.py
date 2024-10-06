c384d4a4bc2bd49ad6eaa7efe9914eb342575e3e
modichirag <cmodi@flatironinstitute.org>
2024-10-06 00:02:22 -0400

    factorized VI

--- gsmvi/bbvi.py
+++ gsmvi/bbvi.py
@@ -31,6 +31,123 @@ def logp_lr_unstable(y, mean, psi, llambda):
 logp_lr_vmap = jax.vmap(logp_lr_unstable, in_axes=[0, None, None, None])
 
 
+
+class ADVI_Factorized():
+    def __init__(self, D, lp, lp_g=None, jit_compile=True):
+        """
+        Inputs:
+          D: (int) Dimensionality (number) of parameters.
+          lp : Function to evaluate target log-probability distribution
+               whose gradient can be evaluated with jax.grad(lp)
+        """
+        self.D = D
+        self.lp = lp
+        self.lp_g = lp_g
+        self.jit_compile = jit_compile
+        if not jit_compile:
+            print("Not using jit compilation. This may take longer than it needs to.")
+
+
+    def loss_function(self, params, key, batch_size):
+        """
+        Internal function to evaluate negative-ELBO which is the loss function
+        """
+        loc, logscale = params
+        scale = jnp.exp(logscale)
+        q = Normal(loc=loc, scale=scale)
+        #
+        samples = q.sample(key, (batch_size,))
+        logl = jnp.sum(self.lp(samples))
+        logq = jnp.sum(q.log_prob(samples))
+        elbo = logl - logq
+        negelbo = -1. * elbo
+        return negelbo
+
+    
+    def fit(self, key, opt, mean=None, scale=None, batch_size=8, niter=1000, nprint=10, monitor=None, retries=10):
+        """
+        Main function to fit a multivariate Gaussian distribution to the target
+
+        Inputs:
+          key: Random number generator key (jax.random.PRNGKey)
+          mean : Optional, initial value of the mean. Expected None or array of size D
+          cov : Optional, initial value of the covariance matrix. Expected None or array of size DxD
+          batch_size : Optional, int. Number of samples to match scores for at every iteration
+          niter : Optional, int. Total number of iterations
+          nprint : Optional, int. Number of iterations after which to print logs
+          monitor : Optional. Function to monitor the progress and track different statistics for diagnostics.
+                    Function call should take the input tuple (iteration number, [mean, cov], lp, key, number of grad evals).
+                    Example of monitor class is provided in utils/monitors.py
+        Returns:
+          mu : Array of shape D, fit of the mean
+          cov : Array of shape DxD, fit of the covariance matrix
+        """
+
+        if self.jit_compile:    # I don't remember why we need to jit here. For monitor?
+            lossf = jit(self.loss_function, static_argnums=(2))
+        else:
+            lossf = self.loss_function
+
+        def opt_step(params, opt_state, key):
+            loss, grads = jax.value_and_grad(lossf, argnums=0)(params, key, batch_size=batch_size)
+            updates, opt_state = opt.update(grads, opt_state, params)
+            params = optax.apply_updates(params, updates)
+            return params, opt_state, loss
+
+        if self.jit_compile: opt_step = jit(opt_step)
+
+        if mean is None:
+            mean = jnp.zeros(self.D)
+        if scale is None:
+            scale  = np.ones(self.D)
+            
+        logscale = np.log(scale)
+        params = (mean, logscale)
+
+        # run optimization
+        opt_state = opt.init(params)
+        losses = []
+        nevals = 1
+
+        for i in range(niter + 1):
+            if(i%(niter//nprint)==0):
+                print(f'Iteration {i} of {niter}')
+            if monitor is not None:
+                if (i%monitor.checkpoint) == 0:
+                    mean = params[0]
+                    logscale = params[1]
+                    cov = np.diag(np.exp(scale))
+                    monitor(i, [mean, cov], self.lp, key, nevals=nevals)
+                    nevals = 0
+
+            j = 0
+            while True:         # Sometimes run crashes due to a bad sample. Avoid that by re-trying.
+                try:
+                    key, _ = random.split(key)
+                    params, opt_state, loss = opt_step(params, opt_state, key)
+                    nevals += batch_size
+                    break
+                except Exception as e:
+                    if j < retries :
+                        j += 1
+                        print(f"Failed with exception {e}")
+                        print(f"Trying again {j} of {retries}")
+                    else : raise e
+            losses.append(loss)
+
+
+        # Convert back to mean and covariance matrix
+        mean = params[0]
+        logscale = params[1]
+        cov = np.diag(np.exp(scale))
+        if monitor is not None:
+            monitor(i, [mean, cov], self.lp, key, nevals=nevals)
+
+        return mean, np.exp(logscale), losses
+
+
+
+############
 class BBVI():
     def __init__(self, D, lp, lp_g=None, jit_compile=True):
         """
@@ -369,7 +486,7 @@ class ADVI_LR(BBVI):
         if llambda is None:
             llambda = np.random.normal(0, 1, size=(self.D, K)) 
         if psi is None:
-            psi = 1+np.random.random(self.D)
+            psi = np.random.random(self.D)
 
         params = (mean, psi, llambda)
 
